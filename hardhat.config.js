5e61faff0cdd06d4b26f10c7cb1b59067b86a332
Jo An <jo@hypotenuse.ai>
2025-11-01 18:38:41 +0800

    feat: compile smart contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,34 +4,13 @@ require("@nomiclabs/hardhat-ethers");
 // Import dotenv module to access variables stored in the .env file
 require("dotenv").config();
 
-// Define Hardhat tasks here, which can be accessed in our test file (test/rpc.js) by using hre.run('taskName')
-task("show-balance", async () => {
-  const showBalance = require("./scripts/showBalance");
-  return showBalance();
-});
-
-task("deploy-contract", async () => {
-  const deployContract = require("./scripts/deployContract");
-  return deployContract();
-});
-
-task("contract-view-call", async (taskArgs) => {
-  const contractViewCall = require("./scripts/contractViewCall");
-  return contractViewCall(taskArgs.contractAddress);
-});
-
-task("contract-call", async (taskArgs) => {
-  const contractCall = require("./scripts/contractCall");
-  return contractCall(taskArgs.contractAddress, taskArgs.msg);
-});
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   mocha: {
     timeout: 3600000,
   },
   solidity: {
-    version: "0.8.9",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
@@ -40,14 +19,8 @@ module.exports = {
     },
   },
   // This specifies network configurations used when running Hardhat tasks
-  defaultNetwork: "local",
+  defaultNetwork: "testnet",
   networks: {
-    local: {
-      // Your Hedera Local Node address pulled from the .env file
-      url: process.env.LOCAL_NODE_ENDPOINT,
-      // Your local node operator private key pulled from the .env file
-      accounts: [process.env.LOCAL_NODE_OPERATOR_PRIVATE_KEY],
-    },
     testnet: {
       // HashIO testnet endpoint from the TESTNET_ENDPOINT variable in the .env file
       url: process.env.TESTNET_ENDPOINT,
